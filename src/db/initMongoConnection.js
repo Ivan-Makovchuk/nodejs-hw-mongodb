27205bbb1e26724719311519ff02cb5e51e5b0e0
Ivan Makovchuk <macad991@gmail.com>
2025-03-21 22:55:25 +0100

    Fix express import

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -3,8 +3,5 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 
 export function initMongoConnection() {
   const MONGODB_URI = getEnvVar('MONGODB_URI');
-
-  return mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });}
+  return mongoose.connect(MONGODB_URI);
+}
