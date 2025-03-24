28d2a0812b786c4e2661195718308e1cf70164ca
Ivan Makovchuk <macad991@gmail.com>
2025-03-24 10:40:45 +0100

    chan 5

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@ import express from 'express';
 import { getEnvVar } from './utils/getEnvVar.js';
 import pino from 'pino-http';
 import cors from 'cors';
-import contactsRouter from './routers/contacts.js';
+import cookieParser from 'cookie-parser';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import router from './routers/index.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = async () => {
   const app = express();
+  app.use(cookieParser());
 
   app.use(express.json());
   app.use(cors());
@@ -21,12 +23,8 @@ export const setupServer = async () => {
       },
     }),
   );
-  app.get('/', (req, res) => {
-    res.json({ message: "API is working!" });
-  });
-  
 
-  app.use('/contacts', contactsRouter);
+  app.use(router);
 
   app.use(notFoundHandler);
 
