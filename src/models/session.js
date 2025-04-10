acab09e6c5095b9fa75a01c1fea691b4b9e2e033
Ivan Makovchuk <macad991@gmail.com>
2025-04-10 11:44:41 +0200

    chan ыуышщт

--- src/models/session.js
+++ src/models/session.js
@@ -4,7 +4,7 @@ const sessionSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'users',
+      ref: 'User',
       required: true,
     },
     accessToken: {
